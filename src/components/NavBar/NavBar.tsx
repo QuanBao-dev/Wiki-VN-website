e32238b8271e3ebecb533912fd5e675f8c3463de
QuanBao-dev <quy123bao20@gmail.com>
2022-10-15 11:17:38 -0700

    change icon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <ul className="navbar-wrapper">
         <li className="navbar-item">
           <NavLink className={"logo"} to="/">
-            <img src="/sugoi.png" alt="" width={"60px"} height={"60px"} />
+            <img src="/logo.png" alt="" width={"60px"} height={"60px"} />
             <span style={{
               transform:"translateX(-12px)"
             }} >SugoiVisualNovels</span>
