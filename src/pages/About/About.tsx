4bab1c5aa96d78ac6da18fc6e8e408e7203f58a8
QuanBao-dev <quy123bao20@gmail.com>
2022-12-15 12:25:58 -0800

    fixed about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,18 +4,17 @@ const About = () => {
     <div className="about-container">
       <h2>About this website</h2>
       <p>
-        Have you ever felt despair when your
-        favorite Visual Novel will never get translated? Here is the site
-        coming for the rescue. On this site, you can vote for your favorite
-        Visual Novel and your visual novel will get translated soon with machine
-        translation.
+        Have you ever felt despair when your favorite Visual Novel will never
+        get translated? Here is the site coming for the rescue. On this site,
+        you can vote for your favorite Visual Novel and your visual novel will
+        get translated soon with machine translation.
       </p>
       <h2>How do I release all of the translation patches on this site?</h2>
       <p>
-        Basically, I collected multiple tools to extract the text, then create a
-        translation patch by my programing skills with the help of Sugoi Model
-        and then repack it into a patch. Shout out to these people who provide
-        me with all the necessary stuffs to translate:
+        Basically, I collected multiple tools to extract the text files from
+        game, then create translation text files by my programing skills with
+        the help of Sugoi Model and then repack it into a patch. Shout out to
+        these people who provide me with all the necessary stuffs to translate:
         <ul>
           <li>
             MingShiba (Sugoi model). This is his{" "}
@@ -43,7 +42,15 @@ const About = () => {
         There are multiple reasons:
         <ul>
           <li>Your voted VN has already had a English patch for it</li>
-          <li>There's someone else in the process of translating it. Because there's no reason to translate a VN which soon will get a translation</li>
+          <li>
+            There's someone else in the process of translating it. Because
+            there's no reason to translate a VN which soon will get a
+            translation
+          </li>
+          <li>
+            The game is untranslatable because there's no available tool to
+            create the patch
+          </li>
         </ul>
       </p>
     </div>
