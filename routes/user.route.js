aa84ea039152b68f49075908bb521454282ede33
QuanBao-dev <quy123bao20@gmail.com>
2023-01-02 10:47:28 -0800

    done

--- routes/user.route.js
+++ routes/user.route.js
@@ -99,7 +99,9 @@ router.post("/login", async (req, res) => {
   if (result.error) {
     return res.status(400).send({ error: result.error.details[0].message });
   }
-  const { email, password } = req.body;
+  let { email, password } = req.body;
+  email = email.toLocaleLowerCase();
+
   try {
     const user = await userModel.findOne({ email });
     if (!user) {
