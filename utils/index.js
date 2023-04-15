f554eb1f26b54734b904a8b0f69126d363dbdd9a
QuanBao-dev <quy123bao20@gmail.com>
2023-04-14 18:39:43 -0700

    BMC webhook updated

--- utils/index.js
+++ utils/index.js
@@ -1,36 +1,44 @@
-const requester = require('./requester');
+const requester = require("./requester");
 
 class BMC {
-    constructor(access_token) {
-        this.access_token = access_token;
-    }
+  constructor(access_token) {
+    this.access_token = access_token;
+  }
 
-    Supporters(page) {
-        if(!page) return this._sendRequest('supporters');
-        return this._sendRequest(`supporters?page=${page}`);
-    }
+  Subscription(id) {
+    return this._sendRequest(`subscriptions/${id}`);
+  }
 
-    Subscriptions(page) {
-        if(!page) return this._sendRequest('subscriptions');
-        return this._sendRequest(`subscriptions?page=${page}`);
-    }
+  Supporter(id) {
+    return this._sendRequest(`supporters/${id}`);
+  }
 
-    Extras(page) {
-        if(!page) return this._sendRequest('extras');
-        return this._sendRequest(`extras?page=${page}`);
-    }
+  Supporters(page) {
+    if (!page) return this._sendRequest("supporters");
+    return this._sendRequest(`supporters?page=${page}`);
+  }
 
-    async _sendRequest(path) {
-        const response = await requester.get(path, {
-            headers: {
-                Authorization: 'Bearer ' + this.access_token,
-            },
-            validateStatus: function (status) {
-                return status >= 200 && status < 300; // default
-            }
-        });
-        return response.data;                        
-    }
+  Subscriptions(page) {
+    if (!page) return this._sendRequest("subscriptions");
+    return this._sendRequest(`subscriptions?page=${page}`);
+  }
+
+  Extras(page) {
+    if (!page) return this._sendRequest("extras");
+    return this._sendRequest(`extras?page=${page}`);
+  }
+
+  async _sendRequest(path) {
+    const response = await requester.get(path, {
+      headers: {
+        Authorization: "Bearer " + this.access_token,
+      },
+      validateStatus: function (status) {
+        return status >= 200 && status < 300; // default
+      },
+    });
+    return response.data;
+  }
 }
 
 module.exports = BMC;
