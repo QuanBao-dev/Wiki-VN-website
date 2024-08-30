3bb13d6485ed24e1d4bb606e3aa55c733c8fe183
QuanBao-dev <quy123bao20@gmail.com>
2024-08-29 21:28:05 -0700

    censored

--- routes/user.route.js
+++ routes/user.route.js
@@ -177,12 +177,12 @@ router.post("/BMC/", async (req, res) => {
   }
 });
 
-router.post("/patreon/", async (req, res) => {
-  if (req.headers["X-Patreon-Signature"] !== process.env.PATREONSECRETKEY)
-    return res.status(401).send("Unauthorized");
-  console.log(req.body);
-  res.send({ message: "Success" });
-});
+// router.post("/patreon/", async (req, res) => {
+//   if (req.headers["X-Patreon-Signature"] !== process.env.PATREONSECRETKEY)
+//     return res.status(401).send("Unauthorized");
+//   console.log(req.body);
+//   res.send({ message: "Success" });
+// });
 
 router.post("/kofi/", async (req, res) => {
   let data = JSON.parse(req.body.data);
